163c966f8b458e63befb3439faee4fffa95a2fa5
goandreus <andreshtml5@gmail.com>
2020-02-02 19:31:12 -0500

    update

--- constantes/main.go
+++ constantes/main.go
@@ -8,10 +8,24 @@ func main(){
 
 	a = 11111
 	b = 5
-
+	n := "andres"
+	p := "chavez"
 
 	//casting
 	c := a + int(b)
 	fmt.Println(c)
+	fmt.Printf("Hola %s %s %d como estas\n", n , p ,b)
+	fmt.Printf("c es de tipo :%T\n", c )
+	fmt.Printf("b es de tipo :%T\n", b)
+	// prioridad aritmetica
+	//d := 6 + 6*(6 -6)
+
+	//fmt.Println(d)
+
+	//valor cero
+	var nombre  string
+	var numero int
+	var entidad bool
+	fmt.Println(nombre, numero, entidad)
 
 }
